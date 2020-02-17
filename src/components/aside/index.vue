322f6c5714c8c622ea47064111ef0bb31c3569ac
marta1014 <lovemadenghui@126.com>
2020-02-17 21:03:37 +0800

    edit-1a

--- src/components/aside/index.vue
+++ src/components/aside/index.vue
@@ -9,9 +9,9 @@
           <span>导航一</span>
         </template>
         <el-menu-item index="/home/1-1">选项1</el-menu-item>
-        <el-menu-item index="1-2">选项2</el-menu-item>
-        <el-menu-item index="1-3">选项3</el-menu-item>
-        <el-submenu index="1-4">
+        <el-menu-item index="/home/1-2">选项2</el-menu-item>
+        <el-menu-item index="/home/1-3">选项3</el-menu-item>
+        <el-submenu index="/home/1-4">
           <template slot="title">选项4</template>
           <el-menu-item index="1-4-1">选项1</el-menu-item>
         </el-submenu>
